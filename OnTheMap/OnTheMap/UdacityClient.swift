f4532ccdcdf2abc2e65cdda29caf38e8498b0b34
Jonathan Grubb <geekygamer@gmail.com>
2016-04-11 21:46:57 -0500

    created project. added UdacityClient.swift

---
+++ OnTheMap/OnTheMap/UdacityClient.swift
@@ -0,0 +1,39 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Jonathan Grubb on 4/11/16.
+//  Copyright © 2016 Jonathan Grubb. All rights reserved.
+//
+
+import Foundation
+
+class UdacityClient : NSObject {
+    
+    // GET
+    func taskForGETMethod(method: String, var parameters: [String:AnyObject], completionHandlerForGET: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+        
+        /* 1. Set the parameters */
+        
+        /* 2/3. Build the URL, Configure the request */
+        
+        /* 4. Make the request */
+
+            /* GUARD: Was there an error? */
+        
+            /* GUARD: Did we get a successful 2XX response? */
+        
+            /* GUARD: Was there any data returned? */
+        
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+
+        /* 7. Start the request */
+    }
+    
+    // POST
+    // func taskForPOSTMethod
+    
+    // PUT
+    // func taskForPUTMethod
+    
+}
