05139e9787dd114323fbae97924b6540d5d83492
Jonathan Grubb <geekygamer@gmail.com>
2016-04-13 19:28:53 -0500

    completed taskForPOSTMethod()

--- OnTheMap/OnTheMap/UdacityClient.swift
+++ OnTheMap/OnTheMap/UdacityClient.swift
@@ -13,25 +13,58 @@ class UdacityClient : NSObject {
     // shared session
     var session = NSURLSession.sharedSession()
 
-    /* GET
-    func taskForPOSTMethod(method: String, var parameters: [String:AnyObject], jsonBody: String, completionHandlerForPOST: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+    // GET
+    func taskForPOSTMethod(method: String, parameters: [String:AnyObject], jsonBody: String, completionHandlerForPOST: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
         
         // 1. Set the parameters
+        // none to set right now...
         
         // 2/3. Build the URL, Configure the request
+        let request = NSMutableURLRequest(URL: udacityURLFromParameters(parameters, withPathExtension: method))
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = jsonBody.dataUsingEncoding(NSUTF8StringEncoding)
         
         // 4. Make the request
-
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            func sendError(error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForPOST(result: nil, error: NSError(domain: "taskForPOSTMethod", code: 1, userInfo: userInfo))
+            }
+            
             // GUARD: Was there an error?
-        
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
             // GUARD: Did we get a successful 2XX response?
-        
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
             // GUARD: Was there any data returned?
-        
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            // Take the udacity header off the response
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+            
             // 5/6. Parse the data and use the data (happens in completion handler)
-
+            self.convertDataWithCompletionHandler(newData, completionHandlerForConvertData: completionHandlerForPOST)
+        }
+        
         // 7. Start the request
-    } */
+        task.resume()
+        
+        return task
+    }
     
     // POST
     // func taskForPOSTMethod
@@ -39,4 +72,35 @@ class UdacityClient : NSObject {
     // DELETE (this is only needed for the Facebook optional api)
     // func taskForDELETEMethod
     
+    // given raw JSON, return a usable Foundation object
+    private func convertDataWithCompletionHandler(data: NSData, completionHandlerForConvertData: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject!
+        do {
+            parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(result: nil, error: NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(result: parsedResult, error: nil)
+    }
+
+    // create a URL from parameters
+    private func udacityURLFromParameters(parameters: [String:AnyObject], withPathExtension: String? = nil) -> NSURL {
+        
+        let components = NSURLComponents()
+        components.scheme = "https"
+        components.host = "www.udacity.com"
+        components.path = "/api" + (withPathExtension ?? "")
+        components.queryItems = [NSURLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = NSURLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.URL!
+    }
+
 }
